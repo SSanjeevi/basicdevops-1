fa5125c1aa467345d5b4b00e3dc56e2cb5e1e5df
Sanjeevi Subramani <ssanjeevi.ss@gmail.com>
2021-11-04 11:14:56 +0530

    Basic devops part 1 and 2

---
+++ ConsoleAppTestBuild/ConsoleAppTestBuild/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleAppTestBuild
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World developer 1!");
+        }
+    }
+}
